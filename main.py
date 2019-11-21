d76c011f6a907cd244585922436bc930a71582aa
Justinas Rumsevicius <justinas.rumsevicius@cern.ch>
2019-11-21 18:36:09 +0100

    Add campaign groups, attributes to root and MiniAOD requests

--- main.py
+++ main.py
@@ -10,60 +10,161 @@ app = Flask(__name__,
             static_folder="./html/static",
             template_folder="./html")
 api = Api(app)
+pwgs = ["B2G",
+        "BPH",
+        "BRI",
+        "BTV",
+        "CPF",
+        "CSC",
+        "DTS",
+        "ECA",
+        "EGM",
+        "EXO",
+        "FSQ",
+        "FWD",
+        "GEN",
+        "HCA",
+        "HGC",
+        "HIG",
+        "HIN",
+        "JME",
+        "L1T",
+        "LUM",
+        "MUO",
+        "PPD",
+        "PPS",
+        "RPC",
+        "SMP",
+        "SUS",
+        "TAU",
+        "TOP",
+        "TRA",
+        "TRK",
+        "TSG"]
+
+
+def magic_sort(rows, depth):
+    rows.sort(key=lambda t: tuple(s[0].lower() if isinstance(s[0], str) else s[0] for s in t[0:depth]))
+
+
+def add_counters(rows):
+    new_rows = []
+    for r in rows:
+        new_row = []
+        for i in r:
+            new_row.append([i, 1])
+
+        new_rows.append(tuple(new_row))
+
+    return new_rows
+
+
+def magic(rows, depth):
+    for index in range(len(rows) - 1, 0, -1):
+        for length in range(depth, 0, -1):
+            if tuple(x[0] for x in rows[index][0:length]) == tuple(x[0] for x in rows[index - 1][0:length]):
+                for i in range(0, length):
+                    rows[index - 1][i][1] += rows[index][i][1]
+                    rows[index][i][1] = 0
+
+                break
+
+
+def split_chained_request_name(name):
+    if name == '':
+        return ''
+
+    spl = name.split('-')
+    return '%s-...-%s' % (spl[0], spl[-1])
 
 
 @app.route('/')
-@app.route('/<string:campaign_name>')
-def index(campaign_name=None):
+def index():
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
-    campaigns = {r[0]: r[1] for r in c.execute('SELECT campaign, COUNT(1) FROM samples GROUP BY campaign')}
     fullname = '%s (%s - %s)' % (request.headers.get('Adfs-Fullname', '???'),
                                  request.headers.get('Adfs-Login', '???'),
                                  request.headers.get('Adfs-Email', '???'))
+
+    campaigns = c.execute('SELECT campaign_group, campaign, COUNT(1) FROM samples GROUP BY campaign')
+    campaigns = add_counters(campaigns)
+    magic_sort(campaigns, 2)
+    magic(campaigns, 2)
+    return render_template('index.html',
+                           campaigns=campaigns,
+                           campaign_groups=sorted(set(x[0][0] for x in campaigns)),
+                           pwgs=pwgs,
+                           fullname=fullname)
+
+
+@app.route('/campaign/<string:campaign_name>')
+@app.route('/campaign/<string:campaign_name>/<string:pwg>')
+@app.route('/campaign_group/<string:campaign_group>')
+@app.route('/campaign_group/<string:campaign_group>/<string:pwg>')
+def campaign_page(campaign_name=None, campaign_group=None, pwg=None):
+    if not campaign_name and not campaign_group:
+        return index()
+
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    fullname = '%s (%s - %s)' % (request.headers.get('Adfs-Fullname', '???'),
+                                 request.headers.get('Adfs-Login', '???'),
+                                 request.headers.get('Adfs-Email', '???'))
+    sql_args = []
+    sql_query = '''SELECT dataset,
+                          root_request,
+                          root_request_priority,
+                          root_request_total_events,
+                          root_request_status,
+                          ifnull(miniaod, ""),
+                          ifnull(miniaod_priority, ""),
+                          ifnull(miniaod_total_events, 0),
+                          ifnull(miniaod_done_events, 0),
+                          ifnull(miniaod_status, ""),
+                          ifnull(chained_request, ""),
+                          interested_pwgs,
+                          uid FROM samples'''
     if campaign_name:
-        def split_chained_request_name(name):
-            if name == '':
-                return ''
-
-            spl = name.split('-')
-            return '%s-...-%s' % (spl[0], spl[-1])
-
-        rows = c.execute('SELECT dataset, root_request, root_request_priority, ifnull(miniaod, ""), ifnull(chained_request, ""), interested_pwgs, uid FROM samples WHERE campaign = ?', [campaign_name])
-        rows = [([r[0].split('_')[0], 1],  # Short name
-                 [r[0], 1],  # Dataset
-                 [r[1], 1],  # Root request
-                 [r[3], 1],  # Mini
-                 [r[4], 1],  # Chained request
-                 split_chained_request_name(r[4]),  # Short chained request
-                 [x for x in r[5].split(',') if x],  # Interested pwgs
-                 r[6],  # uid
-                 r[2],  # Root request priority
-                ) for r in rows]
-
-        # Short name, Dataset, Root request, MiniAOD, Chained request, Short chained request name, Interested pwgs, uid, Priority
-        rows.sort(key=lambda t: tuple(s[0].lower() if isinstance(s[0], str) else s[0] for s in t[0:5]))
-        for index in range(len(rows) - 1, 0, -1):
-            for length in range(5, 0, -1):
-                if tuple(x[0] for x in rows[index][0:length]) == tuple(x[0] for x in rows[index - 1][0:length]):
-                    for i in range(0, length):
-                        rows[index - 1][i][1] += rows[index][i][1]
-                        rows[index][i][1] = 0
-
-                    break
-
-    conn.close()
-    pwgs = ["B2G","BPH","BRI","BTV","CPF","CSC","DTS","ECA","EGM","EXO","FSQ","FWD","GEN","HCA","HGC","HIG","HIN","JME","L1T","LUM","MUO","PPD","PPS","RPC","SMP","SUS","TAU","TOP","TRA","TRK","TSG"]
-    if campaign_name:
-        return render_template('campaign.html',
-                               campaign=campaign_name,
-                               table_rows=rows,
-                               pwgs=pwgs,
-                               fullname=fullname)
+        sql_query += ' WHERE campaign = ?'
+        sql_args.append(campaign_name)
     else:
-        return render_template('index.html',
-                               campaigns=campaigns,
-                               fullname=fullname)
+        sql_query += ' WHERE campaign_group = ?'
+        sql_args.append(campaign_group)
+        campaign_name = campaign_group + '*'
+
+    if pwg and pwg in pwgs:
+        sql_query += ' AND interested_pwgs LIKE ?'
+        sql_args.append('%%%s%%' % (pwg))
+
+    print(sql_query)
+    print(sql_args)
+    rows = c.execute(sql_query, sql_args)
+    rows = [(r[0].split('_')[0],  # Short name
+             r[0],  # Dataset
+             r[1],  # Root request
+             r[5],  # MiniAOD request
+             r[10],  # Chained request
+             r[2],  # Root request priority
+             r[4],  # Root request status
+             r[6],  # MiniAOD priority
+             r[9],  # MiniAOD status
+             r[7] if r[7] > 0 else r[3],  # Total events
+             r[8] if r[8] > 0 else 0,  # Done events
+             split_chained_request_name(r[10]),  # Short chained request
+             [x for x in r[11].split(',') if x],  # Interested pwgs
+             r[12],  # uid
+             ) for r in rows]
+
+    rows = add_counters(rows)
+    magic_sort(rows, 5)
+    magic(rows, 5)
+    conn.close()
+    return render_template('campaign.html',
+                           campaign=campaign_name,
+                           table_rows=rows,
+                           pwgs=pwgs,
+                           pwg=pwg,
+                           fullname=fullname)
 
 
 @app.route('/update', methods=['POST'])
--- update_data.py
+++ update_data.py
@@ -37,49 +37,100 @@ c = conn.cursor()
 c.execute('''CREATE TABLE samples
              (uid integer PRIMARY KEY AUTOINCREMENT,
               campaign text,
+              campaign_group text,
               chained_request text,
               dataset text,
               root_request text,
               root_request_priority integer,
+              root_request_total_events integer,
+              root_request_status text,
               miniaod text,
+              miniaod_priority integer,
+              miniaod_total_events integer,
+              miniaod_done_events integer,
+              miniaod_status text,
               interested_pwgs text)''')
 conn.commit()
 
 for campaign in campaigns:
+    # This is a very stupid thing
+    campaign_group = campaign.replace('wmLHEGEN', '').replace('wmLHEGS', '').replace('pLHE', '').replace('GEN', '').replace('GS', '')
     requests_in_campaign = mcm.get('requests', query='member_of_campaign=%s' % (campaign))
     for index, request in enumerate(requests_in_campaign):
         print('%s/%s %s' % (index + 1, len(requests_in_campaign), request['prepid']))
-        dataset_name = request['dataset_name']
         if len(request['member_of_chain']) == 0:
-            print('Unchained root request %s' % (request['prepid']))
+            # print('Unchained root request %s' % (request['prepid']))
             chained_request_prepid = None
+            dataset_name = request['dataset_name']
             root_request_prepid = request['prepid']
-            miniaod = None
-            nanoaod = None
-            c.execute('INSERT INTO samples VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)',
+            root_request_priority = request['priority']
+            root_request_total_events = request['total_events']
+            root_request_status = request['status']
+            miniaod_prepid = None
+            miniaod_priority = None
+            miniaod_total_events = None
+            miniaod_done_events = None
+            miniaod_status = None
+            interested_pwgs = ','.join(request['interested_pwg'])
+            c.execute('INSERT INTO samples VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
                       [campaign,
+                       campaign_group,
                        chained_request_prepid,
                        dataset_name,
                        root_request_prepid,
-                       request['priority'],
-                       miniaod,
-                       ''])
+                       root_request_priority,
+                       root_request_total_events,
+                       root_request_status,
+                       miniaod_prepid,
+                       miniaod_priority,
+                       miniaod_total_events,
+                       miniaod_done_events,
+                       miniaod_status,
+                       interested_pwgs])
         else:
             for chained_request_prepid in request['member_of_chain']:
                 chained_request = mcm.get('chained_requests', chained_request_prepid)
                 if chained_request:
                     steps = chained_request_to_steps(chained_request)
                     root_request_prepid = chained_request['chain'][0]
-                    miniaod = steps.get('miniaod')
-                    nanoaod = steps.get('nanoaod')
-                    c.execute('INSERT INTO samples VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)',
+                    root_request = mcm.get('requests', root_request_prepid)
+                    dataset_name = root_request['dataset_name']
+                    root_request_priority = root_request['priority']
+                    root_request_total_events = root_request['total_events']
+                    root_request_status = root_request['status']
+                    miniaod_prepid = steps.get('miniaod')
+                    if miniaod_prepid:
+                        print(miniaod_prepid)
+                        miniaod_request = mcm.get('requests', miniaod_prepid)
+                    else:
+                        miniaod_request = {}
+
+                    miniaod_priority = miniaod_request.get('priority')
+                    miniaod_total_events = miniaod_request.get('total_events')
+                    miniaod_done_events = miniaod_request.get('completed_events')
+                    miniaod_status = miniaod_request.get('status')
+                    if miniaod_request:
+                        interested_pwgs = ','.join(miniaod_request['interested_pwg'])
+                    else:
+                        interested_pwgs = ','.join(root_request['interested_pwg'])
+
+                    c.execute('INSERT INTO samples VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
                               [campaign,
+                               campaign_group,
                                chained_request_prepid,
                                dataset_name,
                                root_request_prepid,
-                               request['priority'],
-                               miniaod,
-                               ''])
+                               root_request_priority,
+                               root_request_total_events,
+                               root_request_status,
+                               miniaod_prepid,
+                               miniaod_priority,
+                               miniaod_total_events,
+                               miniaod_done_events,
+                               miniaod_status,
+                               interested_pwgs])
+
+    conn.commit()
 
 conn.commit()
 conn.close()
