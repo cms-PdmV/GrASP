583d59a1f5e8b1590fd7578864b4e9c34fb98386
Paolo <paolo.gunnellini@cern.ch>
2020-05-01 18:33:34 +0200

    update after J's comments

--- update_ul17based.py
+++ update_ul17based.py
@@ -3,12 +3,10 @@ Module to update interested PWGs across UL 16, 17 and 18
 """
 import logging
 import sys
+import sqlite3
 #pylint: disable=wrong-import-position,import-error
 sys.path.append('/afs/cern.ch/cms/PPD/PdmV/tools/McM/')
 from rest import McM
-from update_data import *
-
-#pylint: enable=wrong-import-position,import-error
 
 # Logger
 logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s', level=logging.INFO)
@@ -47,6 +45,9 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS missing_ul
                   resp_group text NOT NULL)''')
 conn.commit()
 
+cursor.execute('''DELETE FROM missing_ul''')
+conn.commit()
+
 for pwg in pwgs:
     query_ul17 = 'prepid=%s*&member_of_campaign=RunIISummer19UL17MiniAOD' % (pwg)
     requests_ul17 = mcm.get('requests', query=query_ul17)
@@ -60,12 +61,10 @@ for pwg in pwgs:
         present_ul18 = False
         present_ul16 = False
 
-        text_pwg=''
-
-        if request_ul17['interested_pwg']:                                                           
-            text_pwg = ','.join(request_ul17['interested_pwg'])                                      
-        else:                                                                                        
-            text_pwg = request_ul17['prepid'].split('-')[0] 
+        if request_ul17['interested_pwg']:
+            text_pwg = ','.join(request_ul17['interested_pwg'])
+        else:
+            text_pwg = request_ul17['prepid'].split('-')[0]
 
         for request_ul18 in requests_ul18:
             if request_ul18['dataset_name'] == dataset_name:
@@ -78,24 +77,6 @@ for pwg in pwgs:
 
                 present_ul18 = True
 
-        if  not present_ul18:
-
-            chained_request_id = request_ul17['member_of_chain'][0]
-            chained_request = mcm.get('chained_requests', query='prepid=%s' %chained_request_id)
-            root_id = chained_request[0]['chain'][0]
-
-            cursor.execute('INSERT INTO missing_ul VALUES (?, ?, ?, ?, ?, ?, ?)',
-                      [request_ul17['prepid']+'ul18',
-                       request_ul17['dataset_name'],
-                       request_ul17['total_events'],
-                       chained_request_id,#should be chain
-                       root_id, #should be root request
-                       'RunIISummer19UL18',
-                       text_pwg])
-            
-            print ' '
-            print 'Not present in the system - UL18'
-            
         for request_ul16 in requests_ul16:
             if request_ul16['dataset_name'] == dataset_name:
                 request_ul16['interested_pwgs'] = interested_pwgs
@@ -104,26 +85,44 @@ for pwg in pwgs:
                             interested_pwgs)
                 update_response = mcm.update('requests', request_ul16)
                 logger.info('Update response (UL16): %s', update_response)
-        
+
                 present_ul16 = True
 
-        if not present_ul16:
 
-            print ' ' 
-            print 'Not present in the system - UL16'
+        if not present_ul16 or not present_ul18:
 
             chained_request_id = request_ul17['member_of_chain'][0]
             chained_request = mcm.get('chained_requests', query='prepid=%s' %chained_request_id)
             root_id = chained_request[0]['chain'][0]
 
+        else:
+            continue
+
+        if  not present_ul18:
             cursor.execute('INSERT INTO missing_ul VALUES (?, ?, ?, ?, ?, ?, ?)',
-                      [request_ul17['prepid']+'ul16',
-                       request_ul17['dataset_name'],
-                       request_ul17['total_events'],
-                       chained_request_id,#should be chain                                                                                         
-                       root_id, #should be root request 
-                       'RunIISummer19UL16',
-                       text_pwg])
+                           [request_ul17['prepid'],
+                            request_ul17['dataset_name'],
+                            request_ul17['total_events'],
+                            chained_request_id,
+                            root_id,
+                            'RunIISummer19UL18',
+                            text_pwg])
+
+            logger.info('Not present in the system - UL18')
+
+        if not present_ul16:
+
+            cursor.execute('INSERT INTO missing_ul VALUES (?, ?, ?, ?, ?, ?, ?)',
+                           [request_ul17['prepid'],
+                            request_ul17['dataset_name'],
+                            request_ul17['total_events'],
+                            chained_request_id,
+                            root_id,
+                            'RunIISummer19UL16',
+                            text_pwg])
+
+            logger.info('Not present in the system - UL16')
+
 
 conn.commit()
 conn.close()
