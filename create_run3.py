ffd8f717f4b9ac2693a2c8707df63c3228ef8fa7
Justinas Rumševičius <justinasr@users.noreply.github.com>
2020-05-27 14:25:04 +0200

    Revert "Add free text additions for Run3 samples (#24)"
    This reverts commit 32bcc1aa8839b64ef63f40847fb4eaac9276cfbb.

--- create_run3.py
+++ create_run3.py
@@ -1,5 +1,5 @@
 """
-TODO: create database for Run3 planning
+TODO: Document
 """
 import sys
 import sqlite3
@@ -26,7 +26,7 @@ def main():
     cursor = conn.cursor()
     # Create table if it does not exist
     cursor.execute('''CREATE TABLE IF NOT EXISTS run3_samples
-                      (uid integer PRIMARY KEY AUTOINCREMENT,
+                      (prepid text PRIMARY KEY NOT NULL,
                        dataset text NOT NULL,
                        total_events integer NOT NULL,
                        interested_pwgs text)''')
