479c181983f08410975602b8b09fd5422278d7e0
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2020-07-22 14:17:13 +0200

    Turn off debug output

--- update_twiki.py
+++ update_twiki.py
@@ -13,7 +13,7 @@ from request_wrapper import RequestWrapper
 #XSDB pycurl requester instance
 xsdb_request = RequestWrapper()
 # McM instance
-mcm = McM(dev=('--dev' in sys.argv), cookie='cookie.txt', debug=True)
+mcm = McM(dev=('--dev' in sys.argv), cookie='cookie.txt')
 
 # Logger
 logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s', level=logging.INFO)
