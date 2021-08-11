40ae3114ee7f806576f68ef6f93fb0a240135c5d
hkaveh <hessamoddin.kaveh@cern.ch>
2021-08-11 17:53:00 +0200

    update style and indentation

--- frontend/src/components/CustomTableForm.vue
+++ frontend/src/components/CustomTableForm.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-  <div class="form-class-text">
-    <h4 style="padding-left: 30px"> You can start creating your custom table here by checking the UL campaigns you're following: </h4>
-  <div class="form-class" style="text-align: center">
-    <span class="input-class"  v-for="campaign in existingCampaigns" :key="campaign"><label for="campaign.name">{{campaign.name}}</label>
-    <input type="checkbox" v-on:change="generateURL" v-bind:id="campaign.name" v-bind:value="campaign.name" v-model="campaigns"></span>
-  </div>
+    <div class="form-class-text">
+      <h4 style="padding-left: 30px"> You can start creating your custom table here by checking the UL campaigns you're following: </h4>
+      <div class="form-class" style="text-align: center">
+        <span class="input-class"  v-for="campaign in existingCampaigns" :key="campaign.name"><label for="campaign.name">{{campaign.name}}</label>
+        <input type="checkbox" v-on:change="generateURL" v-bind:id="campaign.name" v-bind:value="campaign.name" v-model="campaigns"></span>
+      </div>
   
     <h4 style="padding-left: 30px"><label for="datasetnames">Please enter the dataset names you want to follow in your table separated by a | below with no spaces: </label></h4>
-    <input style="text-align: center; color: grey; width: 60%;background-color: white;border-style: solid;" v-model="datasetnames" v-on:input="generateURL" id="datasetnames" placeholder="TTToHadronic_|ST_t-channel_top_4f_">
+    <input style="text-align: center; border: 1px solid black; width: 60%;background-color: white;border-style: solid;" v-model="datasetnames" v-on:input="generateURL" id="datasetnames" placeholder="TTToHadronic_|ST_t-channel_top_4f_">
     
     <p style="padding-left: 30px">Your custom table is here: <a v-bind:href=url> {{url}}</a> </p>    
-  </div>
+    </div>
   </div>
 </template>
 
@@ -27,9 +27,6 @@ export default {
     datasetnames: '',
     url: 'https://cms-pdmv.cern.ch/grasp/'
     }
-  },
-  created () {
-    this.fetchObjectsInfo();
   },
   methods: {
       generateURL: function() {
