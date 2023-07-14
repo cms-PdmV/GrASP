be9dff22af4e2c42d062cad54829b416d40cb203
Geovanny Gonzalez-Rodriguez <geovanny.gonzalez@cern.ch>
2023-07-14 14:40:34 +0200

    Update scripts: Database credentials and McM Tools
    Set database credentials via environment variables
    Set McM Tools folder via environment variables

--- update_scripts/update_users.py
+++ update_scripts/update_users.py
@@ -3,63 +3,63 @@ Module handles user synchronization with McM
 """
 import argparse
 import logging
+import os
 from utils.grasp_database import Database as GrASPDatabase
 from utils.mcm_database import Database as McMDatabase
-
 
 logger = logging.getLogger()
 
 
-class UserUpdater():
+class UserUpdater:
     """
     User updater
     """
 
     def __init__(self, dev):
-        self.mcm_user_db = McMDatabase('users', dev=dev)
-        self.user_db = GrASPDatabase('users')
+        self.mcm_user_db = McMDatabase("users", dev=dev)
+        self.user_db = GrASPDatabase("users")
 
     def update(self):
         """
         Copy McM users to GrASP user database
         """
-        logger.info('Updating users')
+        logger.info("Updating users")
         for users in self.mcm_user_db.bulk_yield(100):
             for user in users:
-                username = user['username']
-                role = user['role']
-                logger.info('Updating %s (%s)', username, role)
-                self.user_db.save({'_id': username,
-                                   'username': username,
-                                   'role': role})
+                username = user["username"]
+                role = user["role"]
+                logger.info("Updating %s (%s)", username, role)
+                self.user_db.save({"_id": username, "username": username, "role": role})
 
 
 def main():
     """
     Main function
     """
-    parser = argparse.ArgumentParser(description='GrASP user update script')
-    parser.add_argument('--db_auth',
-                        help='Path to GrASP database auth file')
-    parser.add_argument('--debug',
-                        help='Enable debug logs',
-                        action='store_true')
-    parser.add_argument('--dev',
-                        help='Use McM-Dev',
-                        action='store_true')
+    parser = argparse.ArgumentParser(description="GrASP user update script")
+    parser.add_argument("--db_auth", help="Path to GrASP database auth file")
+    parser.add_argument("--debug", help="Enable debug logs", action="store_true")
+    parser.add_argument("--dev", help="Use McM-Dev", action="store_true")
     args = vars(parser.parse_args())
-    debug = args.get('debug')
-    logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s',
-                        level=logging.DEBUG if debug else logging.INFO)
-    db_auth = args.get('db_auth')
-    dev = args.get('dev')
-    logger.debug('db_auth=%s, dev=%s, debug=%s', db_auth, dev, debug)
-    GrASPDatabase.set_database_name('grasp')
+    debug = args.get("debug")
+    logging.basicConfig(
+        format="[%(asctime)s][%(levelname)s] %(message)s",
+        level=logging.DEBUG if debug else logging.INFO,
+    )
+    db_auth = args.get("db_auth")
+    dev = args.get("dev")
+    logger.debug("db_auth=%s, dev=%s, debug=%s", db_auth, dev, debug)
+    GrASPDatabase.set_database_name("grasp")
     if db_auth:
         GrASPDatabase.set_credentials_file(db_auth)
+    else:
+        # Retrieve credentials from environment variables
+        db_username = os.getenv("DB_USERNAME")
+        db_password = os.getenv("DB_PASSWORD")
+        GrASPDatabase.set_credentials(username=db_username, password=db_password)
 
     UserUpdater(dev=dev).update()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
